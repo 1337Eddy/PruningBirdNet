3d904e9dde94fc68618ac23f9b10dced948efee2
Adrian Furrer <adrian.furrer1999@gmail.com>
2022-08-10 09:04:11 +0200

    split data into three parts

--- data/split_data.py
+++ data/split_data.py
@@ -19,22 +19,22 @@ for bird in folders:
     #print(calls)
     random.shuffle(calls)
     num_train = int(len(calls) * 0.7)
-    num_test = int(len(calls) * 0.3)
+    num_val = int(len(calls) * 0.2)
     train = calls[:num_train]
-    test = calls[num_train:num_train+num_test]
-    val = calls[num_train+num_test:]
+    val = calls[num_train:num_train+num_val]
+    test = calls[num_train+num_val:]
 #    os.mkdir(destination_path + "train" + "/")
  #   os.mkdir(destination_path + "test" + "/")
     #os.mkdir(destination_path + "val" + "/")
     os.mkdir(destination_path + "train" + "/" + bird + "/")
     os.mkdir(destination_path + "test" + "/" + bird + "/")
-    #os.mkdir(destination_path + "val" + "/" + bird + "/")
+    os.mkdir(destination_path + "val" + "/" + bird + "/")
     for file in train:
         shutil.copyfile(source_path + bird + "/" + file + fileformat, destination_path + "train" + "/" +  bird + "/" + file + fileformat)
         #shutil.copyfile(source_path + bird + "/" + file + ".meta.json", destination_path + "train" + "/" +  bird + "/" + file + ".meta.json")
     for file in test:
         shutil.copyfile(source_path + bird + "/" + file + fileformat, destination_path + "test" + "/" + bird + "/" + file + fileformat)
         #shutil.copyfile(source_path + bird + "/" + file + ".meta.json", destination_path + "test" + "/" + bird + "/" + file + ".meta.json")
-    #for file in val:
-    #    shutil.copyfile(source_path + bird + "/" + file + fileformat, destination_path + "val" + "/" + bird + "/" + file + fileformat)
+    for file in val:
+        shutil.copyfile(source_path + bird + "/" + file + fileformat, destination_path + "val" + "/" + bird + "/" + file + fileformat)
         #shutil.copyfile(source_path + bird + "/" + file + ".meta.json", destination_path + "val" + "/" + bird + "/" + file + ".meta.json")
