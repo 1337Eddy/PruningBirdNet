96ac44c5395682d534a70de56d3c9f143bfde652
Adrian Furrer <adrian.furrer1999@gmail.com>
2022-08-10 08:35:35 +0200

    fixed early stopping bug

--- utils/monitor.py
+++ utils/monitor.py
@@ -11,22 +11,21 @@ class Status(Enum):
 class Monitor():
     def __init__(self, patience, early_stopping):
         self.last_val_loss = 100000
-        self.last_val_acc = 100000
+        self.last_val_acc = 0
         self.patience = patience
         self.counter = 0
         self.early_stopping = early_stopping
         self.early_stopping_counter = 0
     
     def update(self, val_loss, val_acc, lr):
-        if (val_loss >= self.last_val_loss and val_acc < self.last_val_acc):
+        if (val_acc < self.last_val_acc):
             self.counter += 1
             self.early_stopping_counter += 1
         else: 
             self.counter = 0
             self.early_stopping_counter = 0
 
-        self.last_val_loss = val_loss
-        self.last_val_acc = val_acc
+            self.last_val_acc = val_acc
         
         if (self.early_stopping_counter >= self.early_stopping):
             print("Early stopping")
@@ -34,7 +33,7 @@ class Monitor():
 
         if (self.counter >= self.patience):
             self.counter = 0
-            print("Reduce Learning rate to {:.12f}".format(lr*0.5))
+            print("Reduce Learning rate to {:.8f}".format(lr*0.5))
             return Status.LEARNING_RATE
         
         return Status.NOTHING
